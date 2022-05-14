dd7a525bc6bae493fa1ecc15e1b9d9f86895dbb4
OmkarKandale <omkarkandale@gmail.com>
2022-05-14 13:52:55 +0530

    added some so called physics ;p

--- car.js
+++ car.js
@@ -5,16 +5,43 @@ class Car {
 		this.width = width;
 		this.height = height;
 
+		this.speed = 0;
+		this.acceleration = 0.2;
+		this.maxSpeed = 3;
+		this.friction = 0.05;
+
 		this.controls = new Controls();
 	}
 
 	update() {
 		if (this.controls.forward) {
-			this.y -= 2;
+			// to check its moving forward
+			this.speed += this.acceleration;
 		}
 		if (this.controls.reverse) {
-			this.y += 2;
+			// to check its moving reverse
+			this.speed -= this.acceleration;
 		}
+
+		if (this.speed > this.maxSpeed) {
+			// to check its moving above maxSpeed
+			this.speed = this.maxSpeed;
+		}
+		if (this.speed < -this.maxSpeed / 2) {
+			// to check its moving in reverse with proper speed and negative sign indicates reverve movement
+			this.speed = -this.maxSpeed / 2;
+		}
+
+		if (this.speed > 0) {
+			// removing friction while increasing speed
+			this.speed -= this.friction;
+		}
+		if (this.speed < 0) {
+			// adding friction while decreasing speed
+			this.speed += this.friction;
+		}
+
+		this.y -= this.speed;
 	}
 
 	draw(ctx) {
