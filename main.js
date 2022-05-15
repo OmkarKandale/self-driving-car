94f92331ace17b38d18b8c1f27ae830d449fc41b
OmkarKandale <omkarkandale@gmail.com>
2022-05-15 15:13:59 +0530

    added road and side lines

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 
 animate();
 
@@ -12,6 +13,7 @@ function animate() {
 	car.update();
 
 	canvas.height = window.innerHeight;
+	road.draw(ctx);
 	car.draw(ctx);
 	requestAnimationFrame(animate);
 }
